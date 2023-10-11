d1d21ab3967d96b0fce125b0c1d0724846758c29
Mateusz Tołpa <tm53837>
2023-10-11 17:36:10 +0200

    lab2

---
+++ Lab2/src/Image.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * @param url
+ * @param name
+ * @return obiekt klasy Image
+ * @throws MalformedURLException
+ * @throws IOException
+ */
+public class Image {
+    public Image getImage(URL url, String name) throws IOException {
+        try {
+            // Something to do
+            return getImage(new URL(url, name));
+        }
+        catch (MalformedURLException e) {
+            return null;
+        }
+    }
+
+
+}
