a5aee9c9935f2a177bbfb55edf91f4dc37077c68
matthewtop <tolpamateusz@gmail.com>
2023-10-11 21:54:03 +0200

    finished all exercises

--- Lab2/src/Bird.java
+++ Lab2/src/Bird.java
@@ -1,14 +1,12 @@
 //public class Bird {
-//
-//
-////    Bird(String name ) {
-////        System.out.println("Bird Name = " + name);
-////    }
-////    Bird( ) {
-////        System.out.println("Constructor Bird.class");
-////        this( ”Starling” );
-////    }
-////    public static void main( String args[ ] ) {
-////        Bird a = new Bird( );
-////    }
+//    Bird(String name) {
+//        System.out.println("Bird Name = " + name);
+//    }
+//    Bird() {
+//        this("Starling");
+//        System.out.println("Constructor Bird.class");
+//    }
+//    public static void main(String[] args) {
+//        Bird a = new Bird();
+//    }
 //}
--- Lab2/src/Example.java
+++ Lab2/src/Example.java
@@ -1,9 +1,31 @@
+//public class Example
+//{
+//    public static void main( String[] args )
+//    {
+//        Example e1 = new Example( );
+//    // TODO: wyświetl nazwę typu (wyłącznie) dla e1
+//        System.out.println(e1.getClass().getName());
+//    }
+//}
+
+
+class Animal
+{
+    Animal( )
+    {
+        System.out.println( "Konstruktor Animal" );
+    }
+}
+
+class Bird
+{
+    static Animal animal = new Animal( );
+}
 public class Example
 {
-    public static void main( String[] args )
+    public static void main( String...args )
     {
-        Example e1 = new Example( );
-    // TODO: wyświetl nazwę typu (wyłącznie) dla e1
-        System.out.println(e1.getClass().getName());
+        Bird bird1 = new Bird( );
+        Bird bird2 = new Bird( );
     }
 }
