c553a9ffade875e48292a03232f9b1c4678635da
matthewtop <tolpamateusz@gmail.com>
2023-10-26 21:45:53 +0200

    upgraded menu

--- Lab5/src/View.java
+++ Lab5/src/View.java
@@ -1,24 +1,24 @@
 public class View {
     public View(){}
     public void showMainMenu() {
-        System.out.println("|=====================================|");
-        System.out.println("|           Wybierz opcje:            |");
-        System.out.println("|=====================================|");
-        System.out.println("|   1. Wprowadz macierze wejsciowe    |");
-        System.out.println("|   2. Operacje arytmetyczne          |");
-        System.out.println("|   3. Wyswietl macierze              |");
-        System.out.println("|   4. Wyjscie z programu             |");
-        System.out.println("|=====================================|");
+        System.out.println("|===================================================|");
+        System.out.println("|                   Wybierz opcje:                  |");
+        System.out.println("|===================================================|");
+        System.out.println("|         1. Wprowadz macierze wejsciowe            |");
+        System.out.println("|         2. Operacje arytmetyczne                  |");
+        System.out.println("|         3. Wyswietl macierze                      |");
+        System.out.println("|         4. Wyjscie z programu                     |");
+        System.out.println("|===================================================|");
         System.out.println();
         System.out.print("Twoj wybor:    ");
 
     }
     public void showSecondMenu() {
-        System.out.println("|=====================================|");
-        System.out.println("|   1. Transponuj macierze wejsciowe  |");
-        System.out.println("|   2. Pomnoz macierze wejsciowe      |");
-        System.out.println("|   3. Transponuj macierz wynikowa    |");
-        System.out.println("|=====================================|");
+        System.out.println("|===================================================|");
+        System.out.println("|           1. Transponuj macierze wejsciowe        |");
+        System.out.println("|           2. Pomnoz macierze wejsciowe            |");
+        System.out.println("|           3. Transponuj macierz wynikowa          |");
+        System.out.println("|===================================================|");
         System.out.println();
         System.out.print("Twoj wybor:    ");
     }
